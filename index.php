27648599459be508357fbfe748ba1c39ec2602ce
Ben Braunstein <benzbraunstein@gmail.com>
2019-06-11 23:49:25 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,135 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Jonathan Grover - Artist</title>
+  <META NAME="description" content="Jonathan Grover - Artwork, resume, biography, and list of exhibitions">
+	<META NAME="keywords" content="jon, jonathan, grover, john, design, web, graphic, web design, portfolio, work, artist, illustration, photography, multimedia, branding, identity, logos, artwork, teaching, teacher, New York, based, sfai, San Francisco Art Institute, interactive, media, technology, codepajamas">
+	<link rel="shortcut icon" href="/favicon.ico">
+  <link href="css/lightbox.min.css" rel="stylesheet">
+  <link rel="stylesheet" href="css/main.css">
+	<meta name="Owner" content="Jonathan Grover">
+  <meta property="og:title" content="Jonathan Grover" />
+  <meta property="og:type" content="website" />
+  <meta property="og:url" content="https://jonathangrover.com/" />
+  <meta property="og:image" content="https://jonathangrover.com/images/social.jpg" />
+  <meta property="og:description" content="Interactive artwork, resume, biography, and list of exhibitions for Jonathan Grover" />
+  <meta property="fb:app_id" content="595951553898072" />
+	<meta name="robots" content="index,follow">
+	<meta name="Googlebot" content="index,follow">
+  <meta name="Googlebot-Image" content="index,follow">
+  <meta name="verify-v1" content="hKeqO67Ge/cDKmFu1RTliKhwKc3PzDPHmedoqgpmVgQ=">
+</head>
+<body>
+<nav>
+	<h1>Jonathan Grover</h1>
+	<!-- <a class="babyblue" href="?p=about">About</a> -->
+	<a href="#" id="nest1trigger" class="babyblue">Artwork</a>
+	<div id="nest1" class="nest">
+		<a href="?p=turquoise-wind" class="graysky">Turquoise Wind</a>
+		<a href="?p=chakra-synth" class="graysky">Chakra Synth</a>
+		<a href="?p=bridge" class="graysky">Bridge</a>
+		<a href="?p=chimes" class="graysky">Chimes</a>
+		<a href="?p=shadow" class="graysky">Shadow</a>
+		<a href="?p=clotheslines" class="graysky">Clotheslines</a>
+		<a href="?p=under-tables" class="graysky">Under Tables</a>
+		<a href="?p=red-push-pull" class="graysky">Red Push Pull</a>
+		<a href="?p=twilight-maps" class="graysky">Twilight Maps</a>
+		<a href="?p=pulse" class="graysky">Pulse</a>
+		<a href="?p=ants" class="graysky">Ants</a>
+	</div><!-- nest1 -->
+	<a class="babyblue" href="?p=bio">Biography</a>
+	<a class="babyblue" href="https://github.com/jongrover" target="_blank">Code</a>
+	<a class="babyblue" href="?p=contact">Contact</a>
+	<a class="babyblue" href="?p=exhibitions">Exhibitions</a>
+	<a class="babyblue" href="?p=press">Press</a>
+	<a class="babyblue" href="?p=resources">Resources</a>
+	<a class="babyblue" href="?p=resume">Résumé</a>
+	<a class="babyblue" href="?p=statement">Statement</a>
+	<a class="babyblue" href="https://codepajamas.com" target="_blank">Teaching</a>
+</nav>
+<main>
+  <div id="details">
+    <table class="label">
+      <tr>
+        <th>Title</th>
+        <td>Turquoise Wind</td>
+      <tr>
+        <th>Date</th>
+        <td>2018</td>
+      </tr>
+      <tr>
+        <th>Medium</th>
+        <td>Sculpture: Interactive ceramic chimes</td>
+      </tr>
+      <tr>
+        <th>Dimensions</th>
+        <td>8m x 6m x 4.5m</td>
+      </tr>
+      <tr>
+        <th>Description</th>
+        <td>A collaboration with artist Hui-Ying Tsai, this piece is an interactive ceramic chime that plays the sound of the nature/culture of the Pacific islands. The soundscape triggered by the viewers touching the pieces transcends the space and time, and connect the viewers to the land, the ocean, and the past. This piece honors the Austronesian culture and expresses human’s respect to nature since the prehistoric era across all cultures.</td>
+      </tr>
+    </table>
+  </div><!-- details -->
+
+  <div id="video">
+    <div class="videoWrapper2">
+      <iframe width="788" height="445" src="https://www.youtube-nocookie.com/embed/b0M8fjj_0rg?rel=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
+  </div><!-- video -->
+
+  <div id="audio">
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/538455645&color=%233d3635&amp;amp;auto_play=false&amp;amp;hide_related=true&amp;amp;show_comments=false&amp;amp;show_user=false&amp;amp;show_reposts=false&amp;show_artwork=false"></iframe>
+
+    <iframe width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/538455639&color=%233d3635&amp;amp;auto_play=false&amp;amp;hide_related=true&amp;amp;show_comments=false&amp;amp;show_user=false&amp;amp;show_reposts=false&amp;show_artwork=false"></iframe>
+  </div><!-- audio -->
+
+  <div id="photos">
+    <div id="gallery">
+      <div class="pic"><a href="images/turquoise-wind/01.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/01.jpg" alt="pic1"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/02.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/02.jpg" alt="pic2"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/03.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/03.jpg" alt="pic3"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/04.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/04.jpg" alt="pic4"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/05.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/05.jpg" alt="pic5"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/06.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/06.jpg" alt="pic6"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/07.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/07.jpg" alt="pic7"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/08.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/08.jpg" alt="pic8"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/09.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/09.jpg" alt="pic9"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/10.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/10.jpg" alt="pic10"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/11.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/11.jpg" alt="pic11"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/12.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/12.jpg" alt="pic12"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/13.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/13.jpg" alt="pic13"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/14.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/14.jpg" alt="pic14"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/15.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/15.jpg" alt="pic15"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/16.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/16.jpg" alt="pic16"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/17.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/17.jpg" alt="pic17"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/18.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/18.jpg" alt="pic18"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/19.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/19.jpg" alt="pic19"></a></div>
+      <div class="pic"><a href="images/turquoise-wind/20.jpg" data-lightbox="photo" data-title="Jonathan Grover &amp; Hui-Ying Tsai - Turquoise Wind"><img src="images/turquoise-wind/20.jpg" alt="pic20"></a></div>
+    </div>
+  </div><!-- photos -->
+
+</main>
+  <script src="js/jquery.min.js"></script>
+  <script src="js/js.cookie.min.js"></script>
+  <script src="js/imagesloaded.pkgd.min.js"></script>
+  <script src="js/masonry.pkgd.min.js"></script>
+  <script src="js/lightbox.min.js"></script>
+  <script src="js/main.js"></script>
+  <!-- start google analytics -->
+  <script type="text/javascript">
+    var gaJsHost = (("https:" == document.location.protocol) ? "https://ssl." : "http://www.");
+    document.write(unescape("%3Cscript src='" + gaJsHost + "google-analytics.com/ga.js' type='text/javascript'%3E%3C/script%3E"));
+  </script>
+  <script type="text/javascript">
+    try {
+      var pageTracker = _gat._getTracker("UA-3839866-1");
+      pageTracker._trackPageview();
+    } catch(err) {}
+  </script>
+  <!-- end google analytics -->
+</body>
+</html>
